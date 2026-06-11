09e527718f00c8eefebe447a55e6a0335a67b639
tipmangit <qmedsmilanez@tip.edu.ph>
2026-06-11 13:36:58 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_SERVER', 'sql205.infinityfree.com'); 
-define('DB_USERNAME', 'if0_42146942');         
-define('DB_PASSWORD', '814k9Z5SgXC');        
-define('DB_NAME', 'if0_42146942_data_base');      
+define('DB_SERVER', getenv('DB_SERVER'));
+define('DB_USERNAME', getenv('DB_USERNAME'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_NAME', getenv('DB_NAME'));    
 
 ?>
