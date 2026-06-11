650b82cb29d400a7a0cd54ab871d9652b25ea122
tipmangit <qmedsmilanez@tip.edu.ph>
2026-06-12 00:15:54 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
+// Database configuration definitions
+define('DB_SERVER', 'sql205.infinityfree.com'); 
+define('DB_USERNAME', 'if0_42146942');         
+define('DB_PASSWORD', '814k9Z5SgXC');        
+define('DB_NAME', 'if0_42146942_data_base');      
 
-define('DB_SERVER', getenv('DB_SERVER'));
-define('DB_USERNAME', getenv('DB_USERNAME'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_NAME', getenv('DB_NAME'));    
-
+try {
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("ERROR: Connection failure encountered. " . $e->getMessage());
+}
 ?>
